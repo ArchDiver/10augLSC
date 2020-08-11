496d4f2694095ede356a43377a55eadbb30e2162
Leland Crawford <lelandscrawford@gmail.com>
2020-08-10 22:02:49 -0400

    first

---
+++ app.py
@@ -0,0 +1,4 @@
+from fav_five import app
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ fav_five/routes.py
@@ -0,0 +1,14 @@
+from fav_five import app
+from flask import render_template
+
+#home route
+@app.route('/')
+def home():
+    name = "Hot Stuff"
+    return render_template("home.html", name=name,)
+
+#Fav_five route
+@app.route('fav')
+def fav():
+    fav_dict = {1: 'Otis Redding', 2: 'The Beatles', 3: 'Jesse Dee', 4: 'Ray Charles', 5: 'Gorillaz'}
+    return render_template("fav_five.html", fav_dict=fav_dict)
